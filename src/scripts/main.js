7c07c604f63c937999b24de15dfd048e7a86e6e8
Olena Tatyuk <tatyukolena@gmail.com>
2025-05-24 00:05:29 -0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+
+thead.addEventListener('click', (sort) => {
+  const headers = thead.querySelectorAll('th');
+  const index = Array.from(headers).indexOf(sort.target);
+
+  const tbody = document.querySelector('tbody');
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  rows.sort((a, b) => {
+    const aText = a.children[index].textContent;
+    const bText = b.children[index].textContent;
+
+    if (!isNaN(Number(aText)) && !isNaN(Number(bText))) {
+      const aClean = aText.replace(/\$|,/g, '');
+      const bClean = bText.replace(/\$|,/g, '');
+
+      return Number(aClean) - Number(bClean);
+    } else {
+      return aText.localeCompare(bText);
+    }
+  });
+
+  tbody.innerHTML = '';
+  rows.forEach((row) => tbody.appendChild(row));
+});
